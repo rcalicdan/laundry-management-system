776492a7e9fe3debdb130d81d0318922645f6113
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-05 00:28:19 +0800

    Add CustomerPolicy for authorization management on customer models

---
+++ app/Policies/CustomerPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CustomerPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function view(User $user, Customer $customer): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Customer $customer): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Customer $customer): bool
+    {
+        return $user->isAdmin();
+    }
+}
